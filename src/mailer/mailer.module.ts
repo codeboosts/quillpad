b04ed58a203053dae99e1cfb3b37058d31672f79
zeshantech <zeshanshakil0@gmail.com>
2024-03-27 13:16:32 +0500

    feat: mention endpoints

--- src/mailer/mailer.module.ts
+++ src/mailer/mailer.module.ts
@@ -7,7 +7,7 @@ import { MailerModule as NMailerModule } from '@nestjs-modules/mailer';
     NMailerModule.forRoot({
       // TODO: will manage env variable
       // TODO: Here library shows error log instead of success so please don't take it seriously it will be fixed in future
-      transport: 'smtps://zeshanshakil0@gmail.com:wyvt gjmq aimg pzzn@smtp.gmail.com',
+      transport: 'smtps://quillpad@gmail.com:ncdj edzk aqrl lwbp@smtp.gmail.com',
     }),
   ],
 
