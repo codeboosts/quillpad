f57e55bb89ac60261ac5fa5354a02a90c1cecd90
zeshantech <zeshanshakil0@gmail.com>
2024-03-28 17:16:04 +0500

    feat: send grdi

--- src/mailer/mailer.module.ts
+++ src/mailer/mailer.module.ts
@@ -1,50 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MailerService } from './mailer.service';
-import { MailerModule as NMailerModule } from '@nestjs-modules/mailer';
-
-@Module({
-  imports: [
-    NMailerModule.forRoot({
-      // TODO: will manage env variable
-      // TODO: Here library shows error log instead of success so please don't take it seriously it will be fixed in future
-      transport: 'smtps://otpmailer99889898@gmail.com:jznt cnae iazw wevh@smtp.gmail.com',
-    }),
-  ],
-
-  providers: [MailerService],
-  exports: [MailerService],
-})
-export class MailerModule {}
-
-/* import { Module } from '@nestjs/common';
-import { MailerService } from './mailer.service';
-import { MailerOptions, MailerModule as NMailerModule } from '@nestjs-modules/mailer';
+import { SendGridModule } from '@anchan828/nest-sendgrid';
 import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    NMailerModule.forRootAsync({
-      useFactory: async (configService: ConfigService): Promise<MailerOptions> => {
-        console.log(configService.get('SMTP_APP_HOST'), configService.get('SMTP_APP_PORT'), configService.get('SMTP_APP_EMAIL'), configService.get('SMTP_APP_PASSWORD'));
-
-        return {
-          transport: {
-            host: configService.get('SMTP_APP_HOST'),
-            port: 587,
-            secure: false,
-            auth: {
-              user: configService.get('SMTP_APP_EMAIL'),
-              pass: configService.get('SMTP_APP_PASSWORD'),
-            },
-          },
-        };
-      },
+    SendGridModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        apikey: configService.get<string>('SEND_GRID_API_KEY')
+      }),
       inject: [ConfigService],
     }),
   ],
-
   providers: [MailerService],
   exports: [MailerService],
 })
-export class MailerModule {}
- */
+export class MailerModule { }
--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { MailerService as NMailerService } from '@nestjs-modules/mailer';
+import { SendGridService } from '@anchan828/nest-sendgrid';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class MailerService {
-  constructor(private readonly nMailerService: NMailerService) {}
+  constructor(private readonly sendGrid: SendGridService, private readonly configService: ConfigService) { }
 
   async sendOTP(email: string, otp: string): Promise<void> {
     try {
-      await this.nMailerService.sendMail({
+      await this.sendGrid.send({
         to: email,
-        from: 'noreply@example.com',
+        from: this.configService.get<string>('SEND_GRID_FROM_EMAIL'),
         subject: 'OTP Verification',
         html: `<p>Your OTP for email verification is: <strong>${otp}</strong></p>`,
       });
